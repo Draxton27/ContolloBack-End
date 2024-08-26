f9fe70941f75fa0a21a856cf0749e0ff94d091f2
Draxton27 <daniel271204@gmail.com>
2024-08-26 00:34:59 -0600

    Dtos deleted

--- model/CreateNoteDto.cs
+++
@@ -1,8 +0,0 @@
-namespace TechnicalTestApi.Models
-{
-    public class CreateNoteDto
-    {
-        public string Title { get; set; }
-        public string Body { get; set; }
-    }
-}
--- obj/Debug/net8.0/TechincalTestApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/TechincalTestApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechincalTestApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1408d13f49c8d51d530fd522e0189940a5a07d69")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+688403151b2db49e28121b29acfad1fe1b0ee4e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechincalTestApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TechincalTestApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
